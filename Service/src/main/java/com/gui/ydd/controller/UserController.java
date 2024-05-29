4c295e811967bf30d35b022d7d588994a31d9f1d
zjy <3265065465@qq.com>
2024-05-29 14:20:19 +0800

    策略模式集成User操作到userStrategy

--- Service/src/main/java/com/gui/ydd/controller/UserController.java
+++ Service/src/main/java/com/gui/ydd/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.gui.ydd.controller;
 
-import com.gui.ydd.strategy.UserStrategy;
+import com.gui.ydd.strategy.impl.UserStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +12,10 @@ import java.util.Map;
 public class UserController {
 
     @Autowired
-    private Map<String, UserStrategy> authStrategies;
+    private UserStrategy userStrategy;
 
     @PostMapping("/{authType}")
     public Object auth(@PathVariable String authType, @RequestBody Map<String, String> params) {
-        UserStrategy strategy = authStrategies.get(authType);
-        if (strategy == null) {
-            throw new IllegalArgumentException("Invalid auth type: " + authType);
-        }
-        return strategy.execute(authType, params);
+        return userStrategy.execute(authType, params);
     }
 }
--- Service/src/main/java/com/gui/ydd/strategy/impl/UserStrategy.java
+++ Service/src/main/java/com/gui/ydd/strategy/impl/UserStrategy.java
@@ -1,4 +1,108 @@
 package com.gui.ydd.strategy.impl;
 
+import com.gui.ydd.entity.User;
+import com.gui.ydd.factory.ServiceFactory;
+import com.gui.ydd.filter.Filter;
+import com.gui.ydd.filter.LoginValidationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
 public class UserStrategy {
+
+    @Autowired
+    private ServiceFactory serviceFactory;
+
+    private List<Filter> filters;
+
+    public UserStrategy() {
+        filters = new ArrayList<>();
+        filters.add(new LoginValidationFilter());
+    }
+
+    public Object execute(String authType, Map<String, String> params) {
+        switch (authType) {
+            case "updateUser":
+                return updateUser(params);
+            case "register":
+                return register(params);
+            case "login":
+                return login(params);
+            case "deleteUser":
+                return deleteUser(params);
+            default:
+                throw new IllegalArgumentException("Invalid auth type: " + authType);
+        }
+    }
+
+    private Object updateUser(Map<String, String> params) {
+        int userId = Integer.parseInt(params.get("userId"));
+        String account = params.get("account");
+        String username = params.get("username");
+        String password = params.get("password");
+        String role = params.get("role");
+        User user = new User.Builder()
+                .userId(userId)
+                .account(account)
+                .username(username)
+                .password(password)
+                .role(role)
+                .createdate(new Date())
+                .build();
+        serviceFactory.createUserService().updateUser(user);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Update successful");
+        response.put("userId", user.getUserId());
+        return response;
+    }
+
+    private Object register(Map<String, String> params) {
+        String account = params.get("account");
+        String username = params.get("username");
+        String password = params.get("password");
+        String role = params.get("role");
+        User user = new User.Builder()
+                .account(account)
+                .username(username)
+                .password(password)
+                .role(role)
+                .createdate(new Date())
+                .build();
+        user = serviceFactory.createUserService().register(user);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Registration successful");
+        response.put("userId", user.getUserId());
+        return response;
+    }
+
+    private Object login(Map<String, String> params) {
+        for (Filter filter : filters) {
+            filter.execute(params);
+        }
+
+        String account = params.get("account");
+        String password = params.get("password");
+        String role = params.get("role");
+        User user = serviceFactory.createUserService().login(account, password, role);
+        if (user != null) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", "Login successful");
+            response.put("role", user.getRole());
+            return response;
+        } else {
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", "Invalid username or password");
+            return response;
+        }
+    }
+
+    private Object deleteUser(Map<String, String> params) {
+        int userId = Integer.parseInt(params.get("userId"));
+        serviceFactory.createUserService().deleteUser(userId);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Delete user successful");
+        return response;
+    }
 }
